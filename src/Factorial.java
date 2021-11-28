22501e620b7bfb390b1ba616a7b5003195c352f4
José Pedro Unda Montecinos <jose.unda1801@alumnos.ubiobio.cl>
2021-11-28 02:35:05 -0300

    Limpieza, reorganización y comentarios

--- src/Factorial.java
+++ src/Factorial.java
@@ -7,18 +7,23 @@ import java.util.Scanner;
 public class Factorial {
 	public static void main(String[] args) {
 		int num;
-		long t1,t2,valor;
+		long t1,t2;
 		BigInteger valorBig=BigInteger.ZERO;
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Ingrese el número a calcular su factorial:");
 		num = sc.nextInt();
-        
+        /*
+		**Medición del método iterativo
+		*/
         t1 = System.currentTimeMillis();
 		valorBig=factIterativoBig(num);
 		t2 = System.currentTimeMillis();
 		System.out.println("\nEl factorial iterativo de " + num + " es: " + valorBig); 
 		System.out.println("Tiempo de ejecución método iterativo: " + (t2-t1) + " milisegundos.\n");
-        
+        /*
+		**Medición del método recursivo
+		*/
 		t1 = System.currentTimeMillis();
         try{
             valorBig=factRecursivoBig(BigInteger.valueOf(num));
@@ -26,9 +31,11 @@ public class Factorial {
 			System.out.println("El factorial recursivo de " + num + " es: " + valorBig);
 			System.out.println("Tiempo de ejecución método recursivo: " + (t2-t1) + " milisegundos.\n");
         }catch(StackOverflowError e){
-            System.err.println("\nLa recursión se llama demasiadas veces, no es posible calcular el valor del factorial recursivamente debido a que sobre pasa la memoria alocada por la máquina virtual de java.");
+            System.err.println("\nLa recursión se llama demasiadas veces, no es posible calcular el valor del factorial recursivamente debido a que sobrepasa la memoria alocada por la máquina virtual de java.\n");
 		}
-		
+		/*
+		**Medición del método concurrencia
+		*/
 		t1 = System.currentTimeMillis();
 		valorBig=factThreads(num);
 		t2 = System.currentTimeMillis();
@@ -37,53 +44,62 @@ public class Factorial {
         
 		sc.close();
 	}
+	/*
+	**Método iterativo del cálculo de un factorial
+	*/
 	public static BigInteger factIterativoBig(int num) {
-		if(num<0){
-			return BigInteger.valueOf(-1);
-		}
+		//Si el número es negativo, retorna -1 debido a que no se puede calcular el factorial
+		if(num<0) return BigInteger.valueOf(-1);
+
 		BigInteger valor = BigInteger.ONE;
-		for(int i=num;i>0;i--) {
-			//System.out.println(valor + "*" + i);
-			valor=valor.multiply(BigInteger.valueOf(i));
-		}
+		//Va multiplicando de n a 1
+		for(int i=num;i>0;i--) valor=valor.multiply(BigInteger.valueOf(i));
+
 		return valor;
 	} 
-
+	/*
+	**Método recursivo del cálculo de un factorial
+	*/
 	public static BigInteger factRecursivoBig(BigInteger n) {
-		if(n.compareTo(BigInteger.ZERO)<0){
-			return BigInteger.valueOf(-1);
-		}
-        if(n.equals(BigInteger.ONE)||n.equals(BigInteger.ZERO)) {
-            return BigInteger.ONE;
-        }
+		//Si el número es negativo, retorna -1 debido a que no se puede calcular el factorial
+		if(n.compareTo(BigInteger.ZERO)<0) return BigInteger.valueOf(-1);
+
+		//Si el número es 0 o 1, retorna 1 debido a que son los casos base del factorial
+        if(n.equals(BigInteger.ONE) || n.equals(BigInteger.ZERO)) return BigInteger.ONE;
+
+		//El valor de n lo multiplica por el factorial de su antecesor, llamándose a sí mismo
         return n.multiply(factRecursivoBig(n.subtract(BigInteger.ONE))); 
 	} 
-
+	/*
+	**Método de concurrencia (uso de threads) del cálculo de un factorial
+	*/
 	public static BigInteger factThreads(int num) {
 		try{
-			if(num<=20){
-				return factIterativoBig(num);
-			}
-			if(num%2!=0){
-				FactorialThread r = new FactorialThread(num,false);
-				Thread t = new Thread(r);
-				t.start();
-				BigInteger partSol = factIterativoBig((num-1)/2);
-				t.join();
-				return r.res.multiply(partSol); 
-			}else{
-				FactorialThread r = new FactorialThread(num,true);
-				Thread t = new Thread(r);
-				t.start();
-				BigInteger partSol = factIterativoBig(num/2);
-				t.join();
-				return partSol.multiply(r.res);
-			}
+			//si el número es menor a 20, simplemente se calcula el factorial iterativo
+			if(num<=20) return factIterativoBig(num);
+
+			BigInteger partSol;
+			//booleano que dice si el número es par o impar
+			boolean par = num%2!=0;
+			//se hace el llamado al thread que se encargará de calcular la mitad superior del factorial
+			FactorialThread r = new FactorialThread(num,par);
+			Thread t = new Thread(r);
+			t.start();
+			//el segundo thread se encarga de calcular la mitad de abajo del factorial, variando la mitad si es par o impar
+			if(num%2!=0) partSol = factIterativoBig((num-1)/2);
+			else partSol = factIterativoBig((num/2)-1);		
+			t.join();
+			//cuando los dos threads estén listos se retornará la multiplicación entre sus dos resultados, siendo el valor final del factorial
+			return r.res.multiply(partSol);
 		}catch(InterruptedException e){
+			//un thread fue interrumpido por lo que no se pudo realizar el cálculo, se retorna -1
 			return BigInteger.valueOf(-1);
 		}			
 	}
 }
+/*
+**Clase correspondiente al runnable del método para calcular con threads
+*/
 class FactorialThread implements Runnable{
 	int num,limit;
 	boolean par;
@@ -94,12 +110,10 @@ class FactorialThread implements Runnable{
 	}
 	public void run() {
 		res = BigInteger.ONE;
-		if(par){ limit=num/2;}
-		else{limit=(num-1)/2;} 
-
-		for(int i=num;i>limit;i--) {
-			//System.out.println(res + "*" + i);
-			res=res.multiply(BigInteger.valueOf(i));
-		}
+		//se establecen los límites para el cálculo de la mitad superior del factorial
+		if(par) limit=num/2;
+		else limit=(num-1)/2; 
+		//se calcula el factorial como el método iterativo, pero solo hasta el límite establecido
+		for(int i=num;i>limit;i--) res=res.multiply(BigInteger.valueOf(i));
 	} 
 }
