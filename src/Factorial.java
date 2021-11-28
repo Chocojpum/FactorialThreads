2dc3f57dcd7bb5367cd20f1301057edc0de65dc6
José Pedro Unda Montecinos <jose.unda1801@alumnos.ubiobio.cl>
2021-11-27 21:11:46 -0300

    Métodos iterativo y recursivo implementados

--- src/Factorial.java
+++ src/Factorial.java
@@ -9,88 +9,57 @@ public class Factorial {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese el número a calcular su factorial:");
 		num = sc.nextInt();
-		t1 = System.currentTimeMillis();
-		valor=factIterativo(num);
-		t2 = System.currentTimeMillis();
-		System.out.println("El factorial iterativo de " + num + " es: " + valor);
-		System.out.println("Tiempo de ejecución: " + (t2-t1) + " milisegundos.");
-		/*
-		t1 = System.currentTimeMillis();
-		valor=factRecursivo(num);
-		t2 = System.currentTimeMillis();
-		System.out.println("El factorial recursivo de " + num + " es: " + valor);
-		System.out.println("Tiempo de ejecución: " + (t2-t1) + " milisegundos.");
-		*/
-		
-        /*
+        
         t1 = System.currentTimeMillis();
 		valorBig=factIterativoBig(num);
 		t2 = System.currentTimeMillis();
-		System.out.println("El factorial iterativo BIG de " + num + " es: ");
-		System.out.println(valorBig.toString());
-		System.out.println("Tiempo de ejecución: " + (t2-t1) + " milisegundos.");
-		*/
-
+		System.out.println("\nEl factorial iterativo BIG de " + num + " es: " + valorBig); 
+		System.out.println("Tiempo de ejecución método iterativo: " + (t2-t1) + " milisegundos.\n");
         
 		t1 = System.currentTimeMillis();
         try{
             valorBig=factRecursivoBig(BigInteger.valueOf(num));
-        }catch(Error e){
-            System.err.println("caca");
-        }
-		
-		t2 = System.currentTimeMillis();
-		System.out.println("El factorial recursivo BIG de " + num + " es: " + valorBig);
-		System.out.println("Tiempo de ejecución: " + (t2-t1) + " milisegundos.");
-		
+			t2 = System.currentTimeMillis();
+			System.out.println("El factorial recursivo BIG de " + num + " es: " + valorBig);
+			System.out.println("Tiempo de ejecución método recursivo: " + (t2-t1) + " milisegundos.\n");
+        }catch(StackOverflowError e){
+            System.err.println("\nLa recursión se llama demasiadas veces, no es posible calcular el valor del factorial recursivamente.");
+		}
 		
 		sc.close();
 	}
 	public static BigInteger factIterativoBig(int num) {
+		if(num<0){
+			return BigInteger.valueOf(-1);
+		}
 		BigInteger valor = BigInteger.ONE;
 		for(int i=num;i>0;i--) {
 			valor=valor.multiply(BigInteger.valueOf(i));
 		}
 		return valor;
-	}
-	public static long factIterativo(int n) {
-		long valor;
-		if(n>1) {
-			valor=n;
-			for(int i=(int)n-1;i>0;i--)
-					valor*=i;
-			return valor;
-		}else if(n>=0) {
-			return 1;
-		}
-		return -1;
-	}
+	} 
+
 	public static BigInteger factRecursivoBig(BigInteger n) {
+		if(n.compareTo(BigInteger.ZERO)<0){
+			return BigInteger.valueOf(-1);
+		}
         if(n.equals(BigInteger.ONE)||n.equals(BigInteger.ZERO)) {
             return BigInteger.ONE;
         }
         return n.multiply(factRecursivoBig(n.subtract(BigInteger.ONE))); 
-	}
-	public static long factRecursivo(int n) {
-		if(n<0) {
-			return -1;
-		}else if(n==1||n==0) {
-			return 1;
-		}
-		return n*factRecursivo(n-1);
-		
-	}
+	} 
+
 	public static long factThreads(int n) {
-		Runnable r = new Comportamiento(n);
+		Runnable r = new FactorialThread(n);
 		Thread t = new Thread(r);
 		t.start();
 		
 		return -1;			
 	}
 }
-class Comportamiento implements Runnable{
+class FactorialThread implements Runnable{
 	int num;
-	Comportamiento(int num){
+	FactorialThread(int num){
 		this.num=num;
 	}
 	public void run() {
